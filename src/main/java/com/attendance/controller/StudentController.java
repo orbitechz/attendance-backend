4f8d10d50508987811ff3d81ea6797d533e35eff
gusta <gustavopiegat2004@gmail.com>
2025-02-11 21:33:49 -0300

    iniciado ccrud de studant

---
+++ src/main/java/com/attendance/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.attendance.controller;
+
+import com.attendance.entity.Student;
+import com.attendance.repository.StudentRepository;
+import com.attendance.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository repository;
+
+    @Autowired
+    private StudentService service;
+
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getAllStudent() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/students/{id")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+        return ResponseEntity.ok(repository.findById(id).orElse(null));
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        return ResponseEntity.ok(service.create(student));
+    }
+
+}
---
+++ src/main/java/com/attendance/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.attendance.service;
+
+import com.attendance.entity.Student;
+import com.attendance.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    public Student create(Student student){
+
+    }
+}
