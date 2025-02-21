cedf7c8543b411c78e1bd34031a9135f4206f41a
gusta <gustavopiegat2004@gmail.com>
2025-02-20 21:41:37 -0300

    Feat(Security): Implementação spring security

--- src/main/java/com/attendance/entity/Student.java
+++ src/main/java/com/attendance/entity/Student.java
@@ -4,14 +4,18 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Getter @Setter
-public class Student {
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student extends User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,54 +32,4 @@ public class Student {
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     @JsonIgnore
     private List<Attendance> attendances;
-
-    public Student(Long id, String name, String email, String ra, List<Attendance> attendances) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.ra = ra;
-        this.attendances = attendances;
-    }
-
-    public Student() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public @Size(min = 5, max = 50) String getName() {
-        return name;
-    }
-
-    public void setName(@Size(min = 5, max = 50) String name) {
-        this.name = name;
-    }
-
-    public @Email String getEmail() {
-        return email;
-    }
-
-    public void setEmail(@Email String email) {
-        this.email = email;
-    }
-
-    public String getRa() {
-        return ra;
-    }
-
-    public void setRa(String ra) {
-        this.ra = ra;
-    }
-
-    public List<Attendance> getAttendances() {
-        return attendances;
-    }
-
-    public void setAttendances(List<Attendance> attendances) {
-        this.attendances = attendances;
-    }
 }
